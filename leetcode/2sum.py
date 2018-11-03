be497efda59a9f898a757a691dba81b962aee59a
sunyihuan <syh2685@126.com>
2018-11-03 17:56:07 +0800

    lintcode

--- leetcode/2sum.py
+++ leetcode/2sum.py
@@ -6,25 +6,40 @@ created on 2018/11/2
 '''
 
 
+#
+# def twoSum(nums, target):
+#     """
+#     :type nums: List[int]
+#     :type target: int
+#     :rtype: List[int]
+#     """
+#     import numpy as np
+#     x = np.reshape(nums, (len(nums), 1))
+#     y = x + nums
+#
+#     result = np.array(np.where(y == target)).T
+#
+#     return sorted(result[1])
+
 def twoSum(nums, target):
-    """
-    :type nums: List[int]
-    :type target: int
-    :rtype: List[int]
-    """
-    import numpy as np
-    x = np.reshape(nums, (len(nums), 1))
-    y = x + nums
+    hashTable = {}
+    for i in range(len(nums)):
 
-    result = np.array(np.where(y == target)).T
+        subtract_value = target - nums[i]
 
-    return sorted(result[1])
+        if subtract_value in hashTable:
+            return [hashTable[subtract_value], i]
+
+        hashTable[nums[i]] = i
+
 
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
-def addtwonumber(l1,l2):
+
+
+def addtwonumber(l1, l2):
     l1_temp, l2_temp = l1, l2
     final_l = ListNode(None)
     final = final_l
@@ -47,5 +62,37 @@ def addtwonumber(l1,l2):
     final = final.next
     return final
 
-print(addtwonumber([2,3,4], [2,4,5]))
 
+def myPow(x, n):
+    """
+    :type x: float
+    :type n: int
+    :rtype: float
+    """
+    if n == 0:
+        return 1
+    if n < 0:
+        n = -n
+        x = 1 / x
+    if n % 2 == 0:
+        return myPow(x * x, int(n / 2))
+    else:
+        return x * myPow(x * x, int(n / 2))
+
+
+def myAtoi(str):
+    """
+    :type str: str
+    :rtype: int
+    """
+    s = []
+    for i in range(0, len(str) - 1):
+        if str[i] != " ":
+            if str[i] not in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-"]:
+                return 0
+
+
+l1 = [3, 3, 4]
+l2 = [3, 8, 4]
+# print(addtwonumber(l1, l2))
+print(myPow(3.2, -3))
